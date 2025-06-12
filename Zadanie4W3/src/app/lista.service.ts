64ce90ee974a4524346661286021749fb9f9dd31
Krystian Łukowicz <64017868+lakiluk12@users.noreply.github.com>
2025-06-12 16:22:40 +0200

    Add files via upload

--- Zadanie4W3/src/app/lista.service.ts
+++ Zadanie4W3/src/app/lista.service.ts
@@ -1,64 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Film } from '../models/film';
-import { Observable, of } from 'rxjs';
 import { FilmBody } from '../models/film-body';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaService {
-  private static idGen = 1;
+  private readonly apiUrl = 'http://localhost:5003/api/Lista';
 
-  private lista: Film[] = [
-  { id: ListaService.idGen++, tytul: "Incepcja", rezyser: "Christopher Nolan", gatunek: "Sci-Fi", rok_wydania: 2010 },
-  { id: ListaService.idGen++, tytul: "Parasite", rezyser: "Bong Joon-ho", gatunek: "Dramat", rok_wydania: 2019 },
-  { id: ListaService.idGen++, tytul: "Skazani na Shawshank", rezyser: "Frank Darabont", gatunek: "Dramat", rok_wydania: 1994 },
-  { id: ListaService.idGen++, tytul: "Matrix", rezyser: "Lana i Lilly Wachowski", gatunek: "Sci-Fi", rok_wydania: 1999 },
-  { id: ListaService.idGen++, tytul: "Gladiator", rezyser: "Ridley Scott", gatunek: "Historyczny", rok_wydania: 2000 }
-];
+  constructor(private http: HttpClient) {}
 
-  get(): Observable<Film[]> {
-    return of(this.lista);
+  get(titleFilter?: string): Observable<Film[]> {
+    let params = new HttpParams();
+    if (titleFilter) {
+      params = params.set('titleFilter', titleFilter);
+    }
+    return this.http.get<Film[]>(this.apiUrl, { params });
   }
 
   getByID(id: number): Observable<Film> {
-    const ksiazka = this.lista.find(k => k.id === id);
-    if(ksiazka == null) {
-      throw new Error('Nie znaleziono wskazanej książki');
-    }
-    return of(ksiazka);
-  }
-
-  delete(id: number): Observable<void> {
-    this.lista = this.lista.filter(k => k.id !== id);
-    return of(undefined);
-  }
-
-  put(id: number, body: FilmBody): Observable<void> {
-    const ksiazka = this.lista.find(k => k.id === id);
-    if(ksiazka == null) {
-      throw new Error('Nie znaleziono wskazanej książki');
-    }
-
-    ksiazka.rezyser = body.rezyser;
-    ksiazka.gatunek = body.gatunek;
-    ksiazka.rok_wydania = body.rok_wydania;
-    ksiazka.tytul = body.tytul;
-
-    return of(undefined);
+    return this.http.get<Film>(`${this.apiUrl}/${id}`);
   }
 
   post(body: FilmBody): Observable<void> {
-    const ksiazka: Film = {
-      id: ListaService.idGen++,
-      rezyser: body.rezyser,
-      gatunek: body.gatunek,
-      rok_wydania: body.rok_wydania,
-      tytul: body.tytul
-    };
+    return this.http.post<void>(this.apiUrl, body);
+  }
 
-    this.lista.push(ksiazka);
+  put(id: number, body: FilmBody): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, body);
+  }
 
-    return of(undefined);
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
