9a318fed609f475a0f1f923ba1312ffe2e33216f
Krystian Łukowicz <64017868+lakiluk12@users.noreply.github.com>
2025-06-12 16:19:52 +0200

    Add files via upload

---
+++ WebAPI/Models/ListaItem.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Models
+{
+    public class ListaItem
+    {
+        public int Id { get; set; }
+        public string? Tytul { get; set; }
+        public string? Rezyser { get; set; }
+        public string? Gatunek { get; set; }
+        public int Rok_wydania { get; set; }
+    }
+}
